18bfa32f401810af206e60289040ede863e31efc
unknown <souzawesleirodrigues@gmail.com>
2026-05-06 00:14:50 -0300

    feat: preenchimento completo da service do supplier.

--- FrontEnd/src/app/services/supplierService/supplier-service.ts
+++ FrontEnd/src/app/services/supplierService/supplier-service.ts
@@ -1,6 +1,32 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Supplier } from '../../interface/supplier';
 
 @Injectable({
   providedIn: 'root',
 })
-export class SupplierService {}
+export class SupplierService {
+
+  private http = inject(HttpClient);
+  private apiUrl = "http://localhost:8080/api/supplier";
+
+  public findAll() {
+    return this.http.get<Supplier[]>(this.apiUrl);
+  }
+
+  public findById(id: number) {
+    return this.http.get<Supplier>(this.apiUrl+"/"+id);
+  }
+
+  public createSupplier(supplier: Supplier) {
+    return this.http.post<Supplier>(this.apiUrl, supplier);}
+  
+  public updateSupplier(supplier: Supplier) {
+    return this.http.put<Supplier>(this.apiUrl+"/"+supplier.id, supplier);
+  }
+
+  public deleteSupplier(supplier: Supplier) {
+    return this.http.delete(this.apiUrl+"/"+supplier.id);
+  }
+
+  }
