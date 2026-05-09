97150b92edcabe47ccbc0dc10e67d7b1656e36b8
unknown <souzawesleirodrigues@gmail.com>
2026-05-09 04:45:31 -0300

    feat: adicionando rota supplierList no component home.

--- FrontEnd/src/app/components/supplier/supplier-list/supplier-list.ts
+++ FrontEnd/src/app/components/supplier/supplier-list/supplier-list.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-supplier-list',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './supplier-list.html',
   styleUrl: './supplier-list.scss',
 })
