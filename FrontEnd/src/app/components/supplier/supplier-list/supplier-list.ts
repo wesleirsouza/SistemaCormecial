163a56ea387d6be9da434e300273cd7942004935
unknown <souzawesleirodrigues@gmail.com>
2026-05-12 04:42:20 -0300

    feat: criação do component sidebar.

--- FrontEnd/src/app/components/supplier/supplier-list/supplier-list.ts
+++ FrontEnd/src/app/components/supplier/supplier-list/supplier-list.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
-import { RouterLink } from "@angular/router";
+import { ChangeDetectorRef, Component, inject } from '@angular/core';
+import { SupplierService } from '../../../services/supplierService/supplier-service';
+import { Supplier } from '../../../interface/supplier';
+
 
 @Component({
   selector: 'app-supplier-list',
-  imports: [RouterLink],
+  imports: [],
   templateUrl: './supplier-list.html',
   styleUrl: './supplier-list.scss',
 })
-export class SupplierList {}
+export class SupplierList {
+  constructor(private cdr: ChangeDetectorRef){}
+
+  supplierService = inject(SupplierService);
+  listSuppliers: Supplier[] = [];
+
+  ngOnInit(){
+    this.supplierService.findAll().subscribe({
+      next: (data : Supplier[]) => {
+        this.listSuppliers = data;
+        this.cdr.markForCheck();
+      },
+      error: (err) => {
+        console.error('Error fetching suppliers:', err);
+      }
+    })
+  }
+}
