9af8a62aecc16da68cfa18d26163baa8e70ba618
Weslei R de Souza <souzawesleirodrigues@gmail.com>
2026-05-17 14:57:20 -0300

    feat: criação do compoenente delete de supplier.

--- FrontEnd/src/app/components/supplier/delete-supplier/delete-supplier.ts
+++ FrontEnd/src/app/components/supplier/delete-supplier/delete-supplier.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
+import { SupplierService } from '../../../services/supplierService/supplier-service';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { Supplier } from '../../../interface/supplier';
 
 @Component({
   selector: 'app-delete-supplier',
@@ -6,4 +9,17 @@ import { Component } from '@angular/core';
   templateUrl: './delete-supplier.html',
   styleUrl: './delete-supplier.scss',
 })
-export class DeleteSupplier {}
+export class DeleteSupplier {
+  supplierService = inject(SupplierService);
+  activeModal = inject(NgbActiveModal);
+
+  @Input() supplier! : Supplier;
+    
+  deleteSupplier(){
+    this.supplierService.deleteSupplier(this.supplier).subscribe({
+      next: () => {
+        this.activeModal.close()
+      }
+    })
+  }
+}
