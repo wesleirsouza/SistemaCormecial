309c8ac0af39e103c2dd3a011b43320aeaa67f10
Weslei R de Souza <souzawesleirodrigues@gmail.com>
2026-05-18 02:00:32 -0300

    feat: preenchimento do typeScrit do ClientList com metodo openModal.

--- FrontEnd/src/app/components/client/client-list/client-list.ts
+++ FrontEnd/src/app/components/client/client-list/client-list.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, inject } from '@angular/core';
+import { Client } from '../../../interface/client';
+import { ClientService } from '../../../services/client/client';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { CreateClient } from '../create-client/create-client';
 
 @Component({
   selector: 'app-client-list',
@@ -6,4 +10,46 @@ import { Component } from '@angular/core';
   templateUrl: './client-list.html',
   styleUrl: './client-list.scss',
 })
-export class ClientList {}
+export class ClientList {
+
+  constructor(private cdr: ChangeDetectorRef){}
+
+  clientService = inject(ClientService);
+  modalService = inject(NgbModal);
+  listClients: Client[] = [];
+
+  newClient : Client = {
+     id: null,
+    name: '',
+    cnpjCpf: '',
+    email: '',
+    address: {
+      cep: '',
+      street: '',
+      number: '',
+      city: '',
+      state: '',
+      complement: ''
+    }
+  };
+  
+  ngOnInit(): void {
+    this.getClients();
+  }
+  getClients() {
+    throw new Error('Method not implemented.');
+  }
+
+  openModalCreate(
+          dialogSize: 'sm' | 'lg' | 'md' = 'md'
+        ){
+          const modalRef = this.modalService.open(CreateClient, {
+            size: dialogSize,
+            centered: false,
+          });
+          return modalRef.result.then(() => {
+            this.ngOnInit();
+          })
+        }
+
+}
