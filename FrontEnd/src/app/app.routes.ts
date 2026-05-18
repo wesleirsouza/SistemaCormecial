fe79c74c43a1d176c6407c62001b4a3420e60c5b
Weslei R de Souza <souzawesleirodrigues@gmail.com>
2026-05-18 02:04:11 -0300

    feat: adicionando campo cliente no sidebar e a rota no router.

--- FrontEnd/src/app/app.routes.ts
+++ FrontEnd/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { Home } from './components/home/home';
 import { SupplierList } from './components/supplier/supplier-list/supplier-list';
+import { ClientList } from './components/client/client-list/client-list';
 
 export const routes: Routes = [
     {path: '', component: Home, title: 'home page'},
-    {path: 'supplier', component: SupplierList, title: 'supplier'}
+    {path: 'supplier', component: SupplierList, title: 'supplier'},
+    {path: 'client', component: ClientList, title: 'client'}
 ];
